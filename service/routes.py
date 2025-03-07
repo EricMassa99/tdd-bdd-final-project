8b50ee16a51c67c8169e7d448e6fff5d4880ac9b
EricMassa99 <eric.massa123@gmail.com>
2025-03-07 15:48:39 +0100

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -144,3 +144,25 @@ def test_get_product_not_found(self):
 #
 # PLACE YOUR CODE TO DELETE A PRODUCT HERE
 #
+
+######################################################################
+# U P D A T E   A N   E X I S T I N G   P R O D U C T
+######################################################################
+@app.route("/products/<int:product_id>", methods=["PUT"])
+def update_products(product_id):
+    """
+    Update a Product
+
+    This endpoint will update a Product based the body that is posted
+    """
+    app.logger.info("Request to Update a product with id [%s]", product_id)
+    check_content_type("application/json")
+
+    product = Product.find(product_id)
+    if not product:
+        abort(status.HTTP_404_NOT_FOUND, f"Product with id '{product_id}' was not found.")
+
+    product.deserialize(request.get_json())
+    product.id = product_id
+    product.update()
+    return product.serialize(), status.HTTP_200_OK
